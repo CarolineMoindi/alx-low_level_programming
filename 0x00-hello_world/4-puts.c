8c7ea9103604c836a061b34328a15793838320fe
CarolineMoindi <cmoindi18@gmail.com>
2023-02-16 06:05:45 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include (stdio.h>
+		int mai(void)
+		{
+		puts("\"Programming is like bilding a mutlilingual puzzle");
+		return (0);
+		}
