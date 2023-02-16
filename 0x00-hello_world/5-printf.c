103d1b46e5978288811fde1f78af4468101ef950
CarolineMoindi <cmoindi18@gmail.com>
2023-02-16 06:28:52 -0800

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * return: 0 if success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the otucome is a piece of art,\n");
+	return (0);
+}
