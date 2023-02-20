93729c1192c243a6e51cd9ed7751631c8cd7e77d
CarolineMoindi <cmoindi18@gmail.com>
2023-02-20 10:56:15 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 
 	srand(time(0));/* set the seed value based on current time */
 	n = rand() % 200 - 100;/* generate random number between -100 and 100 */
-
+	printf("%d is ", n);
 	if (n > 0)
 	{
 	    printf("%d is positive\n", n);
